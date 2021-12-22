f4fd2d0b4a5cde82de8fad07e244f5abf00256a5
icyi2i <sarthak3gambhir@gmail.com>
2021-12-22 18:19:44 +0000

    Convert TipPop to HOC for accessing trigger elements. Add toggle behaviour based rendering. Add styling, jsdoc and proptypes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
 import React, { useState } from "react";
 import TipPop from "./components/TipPop";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
 
 const App = () => {
   const [show, setShow] = useState(false);
   return (
-    <div>
+    <div className="jumbotron">
       <h1>TipPop</h1>
 
-      <button onClick={() => setShow(!show)}>Toggle Tip</button>
+      <div className="my-5">
+        <p className="mb-3">
+          A tooltip can be mapped to components with custom handler for toggling
+          tooltips!
+        </p>
+        <TipPop tip="A tip to be shown" show={show} behavior="custom">
+          <button
+            className="btn btn-outline-dark"
+            onClick={(e) => {
+              e.stopPropagation();
+              setShow(!show);
+            }}
+          >
+            Toggle Tip
+          </button>
+        </TipPop>
+      </div>
 
-      <TipPop tip="A tip to be shown" show={show} />
+      <div className="my-5">
+        <p className="mb-3">
+          Inbuilt behaviors allow basic toggling of tooltips on hover and click.
+        </p>
+
+        <TipPop
+          tip={
+            <p>
+              The tooltip content can be anything renderable, even{" "}
+              <em>custom</em> <strong>JSX</strong>
+            </p>
+          }
+          behavior="onHover"
+          position="left"
+        >
+          <div className="bg-light square-block">
+            <span>
+              Hover
+              <br />
+              over
+              <br />
+              me!
+            </span>
+          </div>
+        </TipPop>
+      </div>
     </div>
   );
 };
--- src/components/TipPop/index.js
+++ src/components/TipPop/index.js
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./TipPop.scss";
 
-const TipPop = ({
-  show = false,
-  tip = "",
-  position = "auto",
-  disablePointer = false,
-}) => {
-  return <>{show && <div className="position-{position}">{tip}</div>}</>;
+/**
+ * @param JSX tip - text or JSX to render in tooltip
+ * @param bool show - flag to control toggling tooltip on custom behavior
+ * @param bool disablePointer - flag to disable arrow towards the target
+ * @param string behavior - toggle behavior (onHover/onClick/custom)
+ * @param string position - tooltip position (auto/top/bottom/left/right)
+ */
+const TipPop = (props) => {
+  const {
+    tip = "",
+    show = false,
+    behavior = "onHover",
+    position = "auto",
+    disablePointer = false,
+  } = props;
+
+  const [showTip, setShowTip] = useState(false);
+
+  const toggleShowTip = () => setShowTip(!showTip);
+
+  const tipPopContainerEventProps = {
+    onMouseEnter: behavior === "onHover" ? toggleShowTip : undefined,
+    onMouseLeave: behavior === "onHover" ? toggleShowTip : undefined,
+    onClick: behavior === "onClick" ? toggleShowTip : undefined,
+  };
+
+  return (
+    <div className="tippop-container" {...tipPopContainerEventProps}>
+      {props.children}
+      {(behavior === "custom" ? show : showTip) && (
+        <div
+          className={`tippop position-${position} ${
+            disablePointer ? "no-pointer" : ""
+          }`}
+        >
+          {tip}
+        </div>
+      )}
+    </div>
+  );
+};
+
+TipPop.propTypes = {
+  tip: PropTypes.node,
+  show: PropTypes.bool,
+  behavior: PropTypes.oneOf(["onHover", "onClick", "custom"]),
+  position: PropTypes.oneOf(["auto", "top", "bottom", "left", "right"]),
+  disablePointer: PropTypes.bool,
 };
 
 export default TipPop;
